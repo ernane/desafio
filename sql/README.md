36871d8152f27671cb4095cf4a0b2bcd7087d469
Ernane Sena <ernane.sena@gmail.com>
2020-01-30 23:37:18 -0300

    Commit Inicial

---
+++ sql/README.md
@@ -0,0 +1,19 @@
+```sql
+SELECT x.vendedor_nome,
+       x.venda_data,
+       x.venda_valor
+  FROM (
+        SELECT v.venda_id,
+            v.vendedor_id,
+            z.vendedor_nome,
+            v.venda_data,
+            v.venda_valor,
+            rank() OVER (PARTITION BY v.vendedor_id ORDER BY v.venda_valor DESC) AS rank
+          FROM venda v
+          JOIN vendedor z
+            ON z.vendedor_id = v.vendedor_id
+         WHERE EXTRACT(YEAR FROM v.venda_data) = 2016
+       ) x
+ WHERE x.rank = 1
+ORDER BY x.venda_valor DESC;
+```
---
+++ sql/seed.py
@@ -0,0 +1,33 @@
+import random
+import datetime
+
+vendedores = (
+    {"id": 1, "quantidade": 163},
+    {"id": 2, "quantidade": 120},
+    {"id": 3, "quantidade": 260},
+    {"id": 4, "quantidade": 180},
+    {"id": 5, "quantidade": 263},
+    {"id": 6, "quantidade": 130},
+    {"id": 7, "quantidade": 261},
+    {"id": 8, "quantidade": 133},
+)
+
+
+def print_(venda):
+    quantidade = int(venda['quantidade'])
+    vendedor = venda['id']
+    file_name = f"insert_{vendedor}.sql"
+    with open(file_name, 'w') as saida:
+        for x in range(quantidade):
+            valor = round(random.uniform(x, quantidade), 2)
+            data = str(datetime.datetime.strptime(
+                '{} {}'.format(random.randint(1, 366),
+                               random.randint(2015, 2018)), '%j %Y'))
+
+            print(
+                f"INSERT INTO public.venda(vendedor_id, venda_data, venda_valor) VALUES({vendedor}, '{data[0:10]}', {valor});", file=saida)
+
+
+if __name__ == "__main__":
+    for v in vendedores:
+        print_(v)
