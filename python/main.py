4dfe2889e50548b311350330dfa44529e0ae9e08
Ernane Sena <ernane.sena@gmail.com>
2020-02-03 23:02:08 -0300

    Resolvendo a questao 3

--- python/main.py
+++ python/main.py
@@ -42,12 +42,40 @@ def ordena_escadinha(dt_frame):
         return f' Matriz: {dt_frame["index"]} não é Escadinha'
 
 
+def padrao(dt_frame):
+    if not isescadinha(dt_frame["dt"]):
+        rows = [i for i in range(len(dt_frame)-1)]
+
+        for i in rows:
+            correto = False if dt_frame["dt"].iloc[i, -1] != \
+                dt_frame["dt"].iloc[i].max() or dt_frame["dt"].iloc[i, 0] != \
+                dt_frame["dt"].iloc[i+1, 0] else True
+
+        return f' Matrix {dt_frame["index"]} Pradao Correto' if correto else \
+            f' Matrix {dt_frame["index"]} Pradao Inutilizavel'
+    else:
+        return f' Matriz: {dt_frame["index"]} é Escadinha'
+
+
 def print_escadinha(data):
     return f' Matriz: {data["index"]} é Escadinha? {isescadinha(data["dt"])}'
 
 
 if __name__ == '__main__':
     data_frames = (
+        {'index': 'Correta', 'dt': pd.DataFrame(matrizCorreta)},
+        {'index': 'Escadinha', 'dt': pd.DataFrame(matrizEscadinha)},
+        {'index': 'EscadinhaEstendida',
+            'dt': pd.DataFrame(matrizEscadinhaEstendida)},
+        {'index': 'EscadinhaReversa',
+            'dt': pd.DataFrame(matrizEscadinhaReversa)},
+        {'index': 'Inutilizavel_1',
+            'dt': pd.DataFrame(matrizInutilizavel_1)},
+        {'index': 'Inutilizavel_2',
+            'dt': pd.DataFrame(matrizInutilizavel_2)},
+    )
+
+    data_frames_ordem = (
         {'index': 'Correta', 'dt': pd.DataFrame(matrizCorreta)},
         {'index': 'Escadinha', 'dt': pd.DataFrame(matrizEscadinha)},
         {'index': 'EscadinhaEstendida',
@@ -72,5 +100,14 @@ if __name__ == '__main__':
     correta.
     '''
     print("2) Questão")
-    ordena_escadinha = map(ordena_escadinha, data_frames)
+    ordena_escadinha = map(ordena_escadinha, data_frames_ordem)
     [print(i) for i in ordena_escadinha]
+
+    '''
+    Se não houver escadinha, retorne se a matriz está com uma formatação que
+    pode ser utilizada (padrão correto) ou se corresponde a um padrão
+    inutilizável.
+    '''
+    print("3) Questão")
+    padrao_matrix = map(padrao, data_frames)
+    [print(i) for i in padrao_matrix]
